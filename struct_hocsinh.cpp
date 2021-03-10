37b54eefa22984776baa6f6990c7567ff48073a5
Ha-Phuoc-Phuc <80046902+wanbiix@users.noreply.github.com>
2021-03-10 22:58:28 +0700

    not working : dtb_xeploai

--- struct_hocsinh.cpp
+++ struct_hocsinh.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
+#include<cstring>
 using namespace std;
 
 struct hocsinh{
      char hoten[50];
      char xeploai[50];
      float diem, dtb;
-     int somon;
-
+     int somon, a[50];
 };
-void input(struct hocsinh &hs1){
-	int a[20];
+void input(hocsinh &hs){
 	cout<<"Nhap ho ten: ";
-	cin.getline(hs1.hoten, 50);
+	cin.getline(hs.hoten, 50);
 	cout<<"Nhap so mon can tinh ÐTB: ";
-	cin>>hs1.somon;
-	for(int i=1;i<=hs1.somon;i++){
+	cin>>hs.somon;
+	for(int i=1;i<=hs.somon;i++){
 		cout<<"Nhap diem mon "<<i<<": ";
-		cin>>a[i]; 
+		cin>>hs.a[i]; 
 	}
 	cout<<"==================================="<<endl;
+}
+void dtb(hocsinh &hs){
+	for(int i=1; i<=hs.somon;i++){
+		hs.diem=hs.diem+hs.a[i];
+	}
+	hs.dtb=hs.diem/hs.somon;
+}
+void xeploai(hocsinh &hs){
+	if(hs.dtb>=8){
+		strcpy(hs.xeploai, "GIOI");
+	}
+	else if(hs.dtb>=6.5 && hs.dtb<8){
+		strcpy(hs.xeploai, "KHA");
+	}
+	else strcpy(hs.xeploai, "TRUNG BINH");
+}
+void output( hocsinh hs){
+	cout<<"Ho va ten: "<<hs.hoten<<endl;
+	cout<<"Diem trung binh: "<<hs.dtb<<endl;
+	cout<<"Xep loai: "<<hs.xeploai<<endl;
 	
 }
-// void calculator(struct hocsinh hs1){
-// 	for(int i=1;i<=hs1.somon;i++){
-		
-// 	}
-		
-// }
-void output(struct hocsinh hs1){
-	cout<<"Ho va ten: "<<hs1.hoten<<endl;
-	cout<<"Diem trung binh: "<<hs1.dtb<<endl;
-	cout<<"Xep loai: "<<hs1.xeploai<<endl;
-}
-
 int main(){
-	struct hocsinh hs1;
-	input(hs1);
-	output(hs1);
-	
+	struct hocsinh hs;	
+	input(hs);
+	output(hs);	
 }
-
 
